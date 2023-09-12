6368eb171c2b3f70c93820d6d8d5bd7229115bc3
MOHAMMAD AHMAD <119584853+mohammadahmad12222686@users.noreply.github.com>
2023-09-13 02:39:41 +0530

    bubble

---
+++ SOLUTION_USING_JAVA/04_Bubble_Sort.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class 04_Bubble_Sort {
+    static void bubble_sort(int[] arr, int n) {
+      for(int i=0; i<n-1;i++){
+           for(int j=0; j<n-1;j++){
+               if(arr[j]>arr[j+1]){
+                   int temp=arr[j];
+                   arr[j]=arr[j+1];
+                   arr[j+1]=temp;
+
+               }
+           }
+       }
+
+        System.out.println("After bubble sort: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String args[]) {
+        int arr[] = {13, 46, 24, 52, 20, 9};
+        int n = arr.length;
+        System.out.println("Before Using Bubble Sort: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        bubble_sort(arr, n);
+    }
+
+}
